e047bf32ff7248e0da4552e2a5632f35af491306
himanshu.gupta <himanshu.gupta@payu.in>
2020-05-01 02:26:31 +0530

    Code cleanup

--- crashlogger/src/main/java/com/innovate/crashlogger/CrashManager.kt
+++ crashlogger/src/main/java/com/innovate/crashlogger/CrashManager.kt
@@ -34,31 +34,6 @@ object CrashManager {
         setCachePath(context,sentryOptions)
         val sentryClient = SentryClient(sentryOptions, null)
         setupExceptionHandler(sentryClient)
-//        SentryAndroid.init(context.applicationContext,
-//            OptionsConfiguration { options: SentryAndroidOptions ->
-//                options.dsn ="https://a7ccef28c9174ff6baf578798d614d48@o377051.ingest.sentry.io/5198595"
-//                options.isAnrEnabled = true
-//                options.isAnrReportInDebug = false
-//                // Add a callback that will be used before the event is sent to Sentry.
-//                // With this callback, you can modify the event or, when returning null, also discard the event.
-//                options.beforeSend =
-//                    BeforeSendCallback { event: SentryEvent, hint: Any? ->
-//                        if (event.getExceptions() != null && !event.getExceptions()
-//                                .isEmpty() && event.getExceptions().get(0).getModule().contains("com.payu.")
-//                        ) {
-//
-//                            // you can even look up at the the frames or just return the event at this point
-//                            for (item in event.getExceptions().get(0).getStacktrace()
-//                                .getFrames()) {
-//                                if (item.module.contains("com.payu.")) {
-//                                    return@BeforeSendCallback event // send the event
-//                                }
-//                            }
-//                        }
-//                        null
-//                    }
-//            }
-//        )
     }
 
     private fun setCachePath(context: Context, options: SentryAndroidOptions) {
@@ -68,7 +43,6 @@ object CrashManager {
     }
 
     private fun setupExceptionHandler(client: SentryClient) {
-        val currentHandler = Thread.getDefaultUncaughtExceptionHandler()
         if (!(Thread.getDefaultUncaughtExceptionHandler() is CrashExceptionHandler)) {
             Thread.setDefaultUncaughtExceptionHandler(CrashExceptionHandler(client))
         }
