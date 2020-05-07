99341b2c8c059ccc045d5b633ce975ac55a7dd17
himanshu.gupta <himanshu.gupta@payu.in>
2020-05-07 09:32:23 +0530

    Code added for initializing AndroidSerializer

--- crashlogger/src/main/java/com/innovate/crashlogger/CrashExceptionHandler.kt
+++ crashlogger/src/main/java/com/innovate/crashlogger/CrashExceptionHandler.kt
@@ -2,9 +2,16 @@ package com.innovate.crashlogger
 
 import io.sentry.core.SentryClient
 import io.sentry.core.SentryEvent
+import io.sentry.core.SentryLevel
 
-class CrashExceptionHandler(var client: SentryClient) : Thread.UncaughtExceptionHandler {
+class CrashExceptionHandler(var client: SentryClient,var defaultHandler:Thread.UncaughtExceptionHandler) : Thread.UncaughtExceptionHandler {
     override fun uncaughtException(t: Thread, e: Throwable) {
-        client.captureEvent(SentryEvent(e))
+        val event = SentryEvent(e)
+        event.level = SentryLevel.DEBUG
+        client.captureEvent(event)
+        if(null!=defaultHandler) {
+            defaultHandler.uncaughtException(t, e)
+            Thread.setDefaultUncaughtExceptionHandler(defaultHandler)
+        }
     }
 }
--- crashlogger/src/main/java/com/innovate/crashlogger/CrashManager.kt
+++ crashlogger/src/main/java/com/innovate/crashlogger/CrashManager.kt
@@ -1,9 +1,13 @@
 package com.innovate.crashlogger
 
 import android.content.Context
+import android.util.Log
 import io.sentry.android.core.SentryAndroidOptions
+import io.sentry.core.ILogger
+import io.sentry.core.ISerializer
 import io.sentry.core.SentryClient
 import java.io.File
+import java.util.*
 
 
 object CrashManager {
@@ -14,37 +18,62 @@ object CrashManager {
 
     fun createSentryClient(context: Context) {
         var sentryOptions = SentryAndroidOptions();
+        sentryOptions.setSerializer(createSerializerInstance())
+        sentryOptions.isDebug = true
+        sentryOptions.setLogger(createLoggerInstance())
         sentryOptions?.setBeforeSend { event, any ->
-            if (event.getExceptions() != null && !event.getExceptions()
-                    .isEmpty() && event.getExceptions().get(0).getModule().contains("com.payu.")
-            ) {
-
-                // you can even look up at the the frames or just return the event at this point
-                for (item in event.getExceptions().get(0).getStacktrace()
-                    .getFrames()) {
-                    if (item.module.contains("com.innovate.")) {
-                        return@setBeforeSend event // send the event
+            val exceptions = event.exceptions
+            for (e in exceptions){
+                val frames = e.stacktrace.frames
+                for (frame in frames){
+                    if(frame.module.contains("com.innovate")){
+                        return@setBeforeSend event
                     }
                 }
             }
             null // drop the event
         };
         sentryOptions?.dsn =
-            "https://a7ccef28c9174ff6baf578798d614d48@o377051.ingest.sentry.io/5198595"
-        setCachePath(context,sentryOptions)
+                "https://a7ccef28c9174ff6baf578798d614d48@o377051.ingest.sentry.io/5198595"
+        setCachePath(context, sentryOptions)
         val sentryClient = SentryClient(sentryOptions, null)
         setupExceptionHandler(sentryClient)
     }
 
     private fun setCachePath(context: Context, options: SentryAndroidOptions) {
-        val cacheDir = File(context.getCacheDir(), "PayUSentry")
+        val cacheDir = File(context.getCacheDir(), "CustomSentry")
         cacheDir.mkdirs()
         options.cacheDirPath = cacheDir.absolutePath
     }
 
     private fun setupExceptionHandler(client: SentryClient) {
+        var defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
         if (!(Thread.getDefaultUncaughtExceptionHandler() is CrashExceptionHandler)) {
-            Thread.setDefaultUncaughtExceptionHandler(CrashExceptionHandler(client))
+            Thread.setDefaultUncaughtExceptionHandler(CrashExceptionHandler(client,defaultHandler))
         }
+    }
+
+    private fun createSerializerInstance(): ISerializer? {
+        var classLoader = CrashManager::class.java.classLoader
+        val className = "io.sentry.android.core.AndroidSerializer"
+        val customClass = classLoader?.loadClass(className)
+        val constructors = customClass?.declaredConstructors
+        Log.d("payu", Arrays.toString(constructors))
+        val constructor = customClass?.getDeclaredConstructor(ILogger::class.java)
+        constructor?.isAccessible = true
+        var logger=createLoggerInstance()
+        val result = constructor?.newInstance(logger)
+        return result as ISerializer
+    }
+
+    private fun createLoggerInstance(): ILogger {
+        val className = "io.sentry.android.core.AndroidLogger"
+        val customClass = CrashManager::class.java.classLoader?.loadClass(className)
+        val constructors = customClass?.declaredConstructors
+        Log.d("payu", Arrays.toString(constructors))
+        val constructor = customClass?.getDeclaredConstructor()
+        constructor?.isAccessible = true
+        val result = constructor?.newInstance()
+        return result as ILogger
     }
 }
